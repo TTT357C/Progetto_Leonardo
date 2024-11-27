ce8c43cbc3fc31b1e8f24688b0dc43cfb5a8bbb6
Thomas <thomas.causetti@gmail.com>
2024-11-27 16:36:39 +0100

    New Python

--- 5-Gui/LeoNuovoPython/main.py
+++ 5-Gui/LeoNuovoPython/main.py
@@ -1,6 +1,7 @@
 # Import SerialWriter from serial_write_module
 from serial_write_module import SerialWriter as sw
 from Talker import Talker
+from playsound import playsound
 
 def read_api_key():
     try:
@@ -15,8 +16,12 @@ def read_api_key():
 
 api_key = read_api_key()
 
+ja = Talker("ja", "", False)
 
-el1 = Talker("MhCxqsUNqPcgb5bNYy5r", api_key, False)
+it = Talker("jXaluZuLEjmMer7FT0Q2", api_key, False)
+
+el = Talker("MhCxqsUNqPcgb5bNYy5r", api_key, False)
+
 
 def initialize():
     def select_module():
@@ -30,17 +35,17 @@ def initialize():
         # Use match-case to handle module selection
         match module_choice:
             case "1":
-                el1.talk("MQTT module selected")
+                it.talk("Modulo Mqtt in Attivazione")
                 # Import MQTT module
                 from mqtt_read_module import Module as Mod
                 return Mod
             case "2":
-                el1.talk("GUI module selected")
+                it.talk("Modulo Gui in Attivazione")
                 # Import GUI module
                 from gui_read_module import Module as Mod
                 return Mod
             case "3":
-                el1.talk("Keyboard module selected")
+                it.talk("Modulo Tastiera in Attivazione")
                 # Import KEY module
                 from keyboard_read_module import Module as Mod
                 return Mod
@@ -52,12 +57,54 @@ def initialize():
     return select_module()
 
 if __name__ == "__main__":
+
+    playsound("jet2.mp3", block=False)
+
+    """ja.talk("Initializing Robot Systems...")
+
+    ja.talk("Diagnostic Report: Leonardo 3.0")
+
+    ja.talk("Power Core Status: Online and stable.") 
+
+    ja.talk("Fusion reactor at 98% efficiency.")
+
+    ja.talk("Motors: All motors are operational.")
+    ja.talk("Linear Axis: Green across the board.")
+    ja.talk("Gyrosensors: Calibrated and responsive.")
+
+    ja.talk("ALL SYSTEM NOMINAL")
+
+    ja.talk("Starting Voice interface")
+
+    ja.talk("Ready")"""
+    
+    el.talk("Initializing Robot Systems...")
+
+    el.talk("Diagnostic Report: Leonardo 3.0")
+
+    el.talk("Power Core Status: Online and stable.") 
+
+    el.talk("Fusion reactor at 98% efficiency.")
+
+    el.talk("Motors: All motors are operational.")
+    el.talk("Linear Axis: Green across the board.")
+    el.talk("Gyrosensors: Calibrated and responsive.")
+
+    el.talk("ALL SYSTEM NOMINAL")
+
+    el.talk("Starting Italian Voice interface")
+
+    el.talk("Ready")
+
+    it.talk("Leonardo Operativo, in attesa di istruzioni")
+
+    
     # Initialize the selected module
     module = initialize()
     # Create an instance of SerialWriter
     #serial_bridge = sw()
-    el1.talk("Module successfully initialized")
-    el1.talk("Starting serial communication")
+    it.talk("Modulo Attivo")
+    it.talk("Inizio comunicazione seriale")
     if module:
         # Create an instance of the selected module
         module_instance = module()
